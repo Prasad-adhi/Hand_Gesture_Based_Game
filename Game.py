722360ad997ef03686c10c1218be5c6c5825d2d3
Prasad-adhi <68509452+Prasad-adhi@users.noreply.github.com>
2021-06-26 21:28:36 +0530

    Scaled down the grid

--- Game.py
+++ Game.py
@@ -4,15 +4,15 @@ pygame.init()
 screen = pygame.display.set_mode((400, 700))
 clock = pygame.time.Clock()
 done = False
-grid=np.zeros([40,70],dtype=int)
+grid=np.zeros([80,140],dtype=int)
 grid[:,0]=100
-grid[:,69]=-100
+grid[:,138]=-100
 grid[0,:]=2
-grid[39,:]=2
+grid[78,:]=2
 grid=np.transpose(grid)
-grid[10,:]=-1
-grid[60,:]=1
-grid[20,35]=5
+grid[20,:]=-1
+grid[120,10:22]=1
+grid[40,70]=5
 pug_x=np.where(grid==5)[1][0]
 pug_y=np.where(grid==5)[0][0]
 pug_top=False
@@ -28,17 +28,17 @@ while not done:
     if pressed[pygame.K_LEFT]:
         #x -= 3
         if(grid[y][x-1]!=2):
-            grid[60,x:x+4]=0
+            grid[120,x:x+12]=0
             x-=1
-            grid[60,x:x+4]=1
+            grid[120,x:x+12]=1
             x=np.where(grid==1)[1][0]
             
     if pressed[pygame.K_RIGHT]: 
         #x += 3
-        if(grid[y][x+4+1]!=2):
-            grid[60,x:x+4]=0
+        if(grid[y][x+12+1]!=2):
+            grid[120,x:x+12]=0
             x+=1
-            grid[60,x:x+4]=1
+            grid[120,x:x+12]=1
             x=np.where(grid==1)[1][0]
     if(pug_top):
         if(grid[pug_y-1,pug_x]==-1):
@@ -69,8 +69,8 @@ while not done:
             pug_x+=1
             grid[pug_y,pug_x]=5
     screen.fill((0, 0, 0))
-    pygame.draw.rect(screen, color, pygame.Rect(x*10, y*10, 40, 10))
-    pygame.draw.rect(screen, (0,255,0), pygame.Rect(pug_x*10, pug_y*10, 10, 10))
+    pygame.draw.rect(screen, color, pygame.Rect(x*5, y*5, 60, 10))
+    pygame.draw.rect(screen, (0,255,0), pygame.Rect(pug_x*5, pug_y*5, 10, 10))
     pygame.draw.rect(screen, (255,0,0), pygame.Rect(0, 0, 400, 10))#top wall
     pygame.draw.rect(screen, (0,0,255), pygame.Rect(0, 0, 10, 700))#left wall
     pygame.draw.rect(screen, (255,0,0), pygame.Rect(0, 690, 400, 10))#bottom wall
